5d81d6d251d4d4d76b2507be2672658cab2e646b
David Balatero <dbalatero@gmail.com>
2016-01-02 17:52:59 -0800

    WIP parser

--- src/roland_juno_106.js
+++ src/roland_juno_106.js
@@ -1,7 +1,71 @@
-function parse(sysexBuffer) {
-  return {};
-}
-
 export default {
-  parse: parse
+  parse: function(sysex) {
+    let constants = {
+      WAVE_LENGTH_16: 1 << 0,
+      WAVE_LENGTH_8: 1 << 1,
+      WAVE_LENGTH_4: 1 << 2,
+      PULSE_WAVEFORM_SET: 1 << 3,
+      TRIANGLE_WAVEFORM_SET: 1 << 4,
+      CHORUS_ON: 1 << 5,
+      CHORUS_LEVEL: 1 << 6,
+      DCO_PWM_TYPE: 1 << 0,
+      VCA_MOD_TYPE: 1 << 1,
+      VCF_POLARITY: 1 << 2
+    };
+
+    let patch = {
+      "osc": { "osc1": {} },
+      "mod": { "lfo": {} },
+      "filter": {},
+      "envelope": {},
+      "amp": {},
+      "chorus": {}
+    };
+
+    if (sysex[0] !== 0xf0) {
+      throw "Bad sysex starter";
+    }
+
+    if (sysex[1] !== 0x41) {
+      throw "NO ROLAND";
+    }
+
+    patch.mod.lfo.rate = sysex[5];
+    patch.mod.lfo.delay = sysex[6];
+    patch.osc.osc1.lfo = sysex[7];
+    patch.osc.osc1.pwmLevel = sysex[8];
+    patch.osc.osc1.noise = sysex[9];
+    patch.filter.frequency = sysex[10];
+    patch.filter.resonance = sysex[11];
+    patch.filter.envelopeAmount = sysex[12];
+    patch.filter.lfo = sysex[13];
+    patch.filter.keyboardTracking = sysex[14];
+    patch.amp.level = sysex[15];
+    patch.envelope.attack = sysex[16];
+    patch.envelope.decay = sysex[17];
+    patch.envelope.sustain = sysex[18];
+    patch.envelope.release = sysex[19];
+    patch.osc.osc1.subOsc = sysex[20];
+
+    if (sysex[21] & constants.WAVE_LENGTH_16) {
+      patch.osc.osc1.waveLength = '16';
+    } else if (sysex[21] & constants.WAVE_LENGTH_8) {
+      patch.osc.osc1.waveLength = '8';
+    } else if (sysex[21] & constants.WAVE_LENGTH_4) {
+      patch.osc.osc1.waveLength = '4';
+    } else {
+      throw "The wavelength is not set?";
+    }
+
+    patch.osc.osc1.pulseWave = Boolean(sysex[21] & constants.PULSE_WAVEFORM_SET);
+    patch.osc.osc1.triangleWave = Boolean(sysex[21] & constants.TRIANGLE_WAVEFORM_SET);
+    patch.chorus.enabled = !Boolean(sysex[21] & constants.CHORUS_ON);
+    patch.chorus.level = Boolean(sysex[21] & constants.CHORUS_LEVEL) ? 1 : 2;
+
+    patch.osc.osc1.pwmType = Boolean(sysex[22] & constants.DCO_PWM_TYPE) ? 'manual' : 'lfo';
+    // TODO vca env, vcf polarity, hpf position (0 - 3)
+
+    return patch;
+  }
 };
+
--- test/integration/roland_juno_106/sysex_test.js
+++ test/integration/roland_juno_106/sysex_test.js
@@ -1,13 +1,16 @@
 let fs = require('fs');
-console.log(fs.readdirSync('./'));
-let sysexBuffer = fs.readFileSync('./test/fixtures/roland_juno_106/patch_dump.syx');
-let RolandJuno106 = require('./src/roland_juno_106');
+import RolandJuno106 from '../../../src/roland_juno_106';
 
 describe('RolandJuno106', () => {
   describe('sysex parsing', () => {
     it('returns a representation of patch state', () => {
-      let result = RolandJuno106.parse(sysexBuffer);
-      expect(result).to.eq({});
+      let sysexBuffer = fs
+        .readFileSync('./test/fixtures/roland_juno_106/patch_dump.syx');
+
+      let sysex = new Uint8Array(sysexBuffer);
+
+      let result = RolandJuno106.parse(sysex);
+      expect(result).to.eql({});
     });
   });
 });
