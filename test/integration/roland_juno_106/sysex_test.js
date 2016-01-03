bcd08e3b615d962ddc1e5c6880cae1481aab551d
bion <bion@citizencode.io>
2016-01-02 21:53:42 -0800

    Add sysexEncode for RolandJuno106

--- test/integration/roland_juno_106/sysex_test.js
+++ test/integration/roland_juno_106/sysex_test.js
@@ -1,13 +1,41 @@
 let fs = require('fs');
 import RolandJuno106 from '../../../src/roland_juno_106';
 
-describe('RolandJuno106', () => {
-  describe('sysex parsing', () => {
-    it('returns a representation of patch state, input 1', () => {
-      let sysexBuffer = fs
-          .readFileSync('./test/fixtures/roland_juno_106/patch_dump_1.syx');
+describe('RolandJuno106', function () {
+  before(function () {
+    this.sysexOneBuffer =
+      fs.readFileSync('./test/fixtures/roland_juno_106/patch_dump_1.syx');
+    this.sysexTwoBuffer =
+      fs.readFileSync('./test/fixtures/roland_juno_106/patch_dump_2.syx');
 
-      let sysex = new Uint8Array(sysexBuffer);
+    this.sysexTwoData =
+      { osc:
+        { osc1:
+          { lfo: 0,
+            pwmLevel: 104,
+            noise: 127,
+            subOsc: 0,
+            waveLength: '8',
+            pulseWave: true,
+            triangleWave: true,
+            pwmType: 'manual' } },
+        mod: { lfo: { rate: 127, delay: 0 } },
+        filter:
+        { frequency: 127,
+          resonance: 0,
+          envelopeAmount: 127,
+          lfo: 127,
+          keyboardTracking: 0,
+          polarity: 'negative',
+          hpf: 1 },
+        envelope: { attack: 127, decay: 0, sustain: 0, release: 127 },
+        amp: { level: 127, modType: 'gate' },
+        chorus: { enabled: true, level: 1 } };
+  });
+
+  describe('sysex decoding', function () {
+    it('returns a representation of patch state, input 1', function () {
+      let sysex = new Uint8Array(this.sysexOneBuffer);
       let expected =
           { osc:
             { osc1:
@@ -27,45 +55,40 @@ describe('RolandJuno106', () => {
               lfo: 0,
               keyboardTracking: 29,
               polarity: 'negative',
-              hpf: 2 },
+              hpf: 1 },
             envelope: { attack: 0, decay: 60, sustain: 0, release: 127 },
             amp: { level: 105, modType: 'env' },
             chorus: { enabled: true, level: 2 } };
 
-      let result = RolandJuno106.parse(sysex);
+      let result = RolandJuno106.sysexDecode(sysex);
       expect(result).to.eql(expected);
     });
 
-    it('returns a representation of patch state, input 1', () => {
-      let sysexBuffer = fs
-          .readFileSync('./test/fixtures/roland_juno_106/patch_dump_2.syx');
+    it('returns a representation of patch state, input 1', function () {
+      let sysex = new Uint8Array(this.sysexTwoBuffer);
+      let expected = this.sysexTwoData;
+      let result = RolandJuno106.sysexDecode(sysex);
 
-      let sysex = new Uint8Array(sysexBuffer);
-      let expected =
-          { osc:
-            { osc1:
-              { lfo: 0,
-                pwmLevel: 104,
-                noise: 127,
-                subOsc: 0,
-                waveLength: '8',
-                pulseWave: true,
-                triangleWave: true,
-                pwmType: 'manual' } },
-            mod: { lfo: { rate: 127, delay: 0 } },
-            filter:
-            { frequency: 127,
-              resonance: 0,
-              envelopeAmount: 127,
-              lfo: 127,
-              keyboardTracking: 0,
-              polarity: 'negative',
-              hpf: 0 },
-            envelope: { attack: 127, decay: 0, sustain: 0, release: 127 },
-            amp: { level: 127, modType: 'gate' },
-            chorus: { enabled: true, level: 1 } };
-      let result = RolandJuno106.parse(sysex);
-      console.log(result);
+      expect(result).to.eql(expected);
+    });
+  });
+
+  describe('sysex encoding', function () {
+    it('encodes the patch state into sysex', function () {
+      let expected = new Uint8Array(this.sysexTwoBuffer);
+      let input = this.sysexTwoData;
+      let result = RolandJuno106.sysexEncode(input);
+
+      expect(result).to.eql(expected);
+    });
+  });
+
+  describe('sysex encoding and decoding', function () {
+    it('does not change the encoded message', function () {
+      let expected = new Uint8Array(this.sysexTwoBuffer);
+      let decoded = RolandJuno106.sysexDecode(expected);
+      let result = RolandJuno106.sysexEncode(decoded);
+
       expect(result).to.eql(expected);
     });
   });
